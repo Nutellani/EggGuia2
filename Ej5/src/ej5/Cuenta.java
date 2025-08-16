b9757f449f90b7d2f10d3bb129cba1f39591e2f4
Matias Gallerani <galleranimatias@gmail.com>
2025-08-16 20:10:22 -0300

    agregado Ej5 sin terminar

---
+++ Ej5/src/ej5/Cuenta.java
@@ -0,0 +1,20 @@
+package ej5;
+
+public class Cuenta {
+    
+    int numeroCuenta;
+    long DNI;
+    double saldoActual;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(int numeroCuenta, long DNI, double saldoActual) {
+        this.numeroCuenta = numeroCuenta;
+        this.DNI = DNI;
+        this.saldoActual = saldoActual;
+    }
+    
+    
+    
+}
