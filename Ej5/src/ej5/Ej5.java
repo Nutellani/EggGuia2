5cb76b83a95de39b95e8da558a8680625200f778
Matias Gallerani <galleranimatias@gmail.com>
2025-08-25 14:47:25 -0300

    termina2 el ejercicio 5

--- Ej5/src/ej5/Ej5.java
+++ Ej5/src/ej5/Ej5.java
@@ -20,47 +20,80 @@ package ej5;
 import java.util.Scanner;
 
 public class Ej5 {
-    
+
     static Scanner sc = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-        
+
         Cuenta cuenta = createAccount();
         addCash(cuenta);
         retireCash(cuenta);
         fastRetireCash(cuenta); // "extraccion rapida"
+        consultarSaldo(cuenta);
+        consultarDatos(cuenta);
+        
     }
 
-    private static Cuenta createAccount(){
-        
-        System.out.println("Ingrese Numero de cuenta a ingresar ");
+    private static Cuenta createAccount() {
+
+        System.out.println("Ingrese Numero de su cuenta a ingresar ");
         int numeroCuenta = sc.nextInt();
-        
+
         System.out.println("Ahora ingrese su DNI pls :) ");
         long DNI = sc.nextLong();
-        
+
         System.out.println("Por ultimo le pedimos su saldo actual ");
         double saldoActual = sc.nextDouble();
-        
+
         return new Cuenta(numeroCuenta, DNI, saldoActual);
     }
-    
-    private static void addCash(Cuenta cuenta){
-        
+
+    private static void addCash(Cuenta cuenta) {
+
         System.out.println("Ahora ingrese el monto a aumentar");
         double saldoASumar = sc.nextDouble();
         cuenta.addCash(cuenta, saldoASumar);
         System.out.println("Monto aumentado a: " + cuenta.getSaldoActual());
     }
-    
-    private static void retireCash(Cuenta cuenta){
-        
+
+    private static void retireCash(Cuenta cuenta) {
+
         System.out.println("Ahora ingrese el monto a retirar");
         double saldoARestar = sc.nextDouble();
-        cuenta.retireCash(cuenta, saldoARestar);
-        System.out.println("Monto restante es de: " + cuenta.getSaldoActual());
+        
+        System.out.println("El verifyCash(cuenta, saldoARestar) salió: " + (verifyCash(cuenta, saldoARestar)));
+        if (verifyCash(cuenta, saldoARestar)) {
+            cuenta.retireCash(cuenta, saldoARestar);
+            System.out.println("Monto restante es de: " + cuenta.getSaldoActual());
+        } else {
+            System.out.println("Monto a retirar mayor que el saldo lel");
+        }
+    }
+
+    private static boolean verifyCash(Cuenta cuenta, double saldoARestar) {
+        return saldoARestar < cuenta.getSaldoActual();
     }
     
+    private static void fastRetireCash(Cuenta cuenta){
+        System.out.println("EXTRACCIÓN RÁPIDA: Cuanto desea retirar?? ");
+        double saldoARestar = sc.nextDouble();
+        if (saldoARestar > cuenta.saldoActual*0.2) {
+            System.out.println("No puede retirar mas del 20 %");
+            return ;
+        }
+        
+        cuenta.setSaldoActual(cuenta.getSaldoActual()-saldoARestar);
+        System.out.println("Saldo retirado: " + saldoARestar + ", saldo restante: " + cuenta.getSaldoActual());
+    }
     
+    private static void consultarSaldo(Cuenta cuenta){
+        System.out.println("SUS saldo actual ess de: " + cuenta.getSaldoActual());
+    }
     
+    private static void consultarDatos(Cuenta cuenta){
+        System.out.println("Numero de la cuenta: " + cuenta.getNumeroCuenta());
+        System.out.println("DNI de la cuenta: " + cuenta.getDNI());
+        System.out.println("Saldo FINAL de la cuenta: " + cuenta.getSaldoActual());
+        System.out.println(":)");
+    }
 }
